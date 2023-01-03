d31902c7c43e383fea460952d507f2d60bc52fc2
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 15:07:47 -0500

    Update GlobalExceptionHandler.java

--- infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/handlingexceptions/GlobalExceptionHandler.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/handlingexceptions/GlobalExceptionHandler.java
@@ -4,16 +4,22 @@ import co.com.astropay.usecase.exceptions.Response;
 import co.com.astropay.usecase.exceptions.custom.BadRequestException;
 import co.com.astropay.usecase.exceptions.custom.CustomException;
 import co.com.astropay.usecase.exceptions.custom.NoContentException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@ControllerAdvice
+@ControllerAdvice("co.com.astropay.api")
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {CustomException.class})
@@ -25,8 +31,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .descripcionRespuesta(ex.getMessage())
                 .result(Collections.emptyList())
                 .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
 }
     @ExceptionHandler(value = {RuntimeException.class})
     public ResponseEntity<Response> custom(RuntimeException ex){
@@ -37,7 +42,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .descripcionRespuesta(ex.getMessage())
                 .result(Collections.emptyList())
                 .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {NoContentException.class})
@@ -48,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .descripcionRespuesta(ex.getMessage())
                 .result(Collections.emptyList())
                 .build();
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+        return new ResponseEntity<Response>(response, HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler(value = {BadRequestException.class})
@@ -60,8 +65,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .descripcionRespuesta(ex.getMessage())
                 .result(Collections.emptyList())
                 .build();
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+        return new ResponseEntity<Response>(response, HttpStatus.NO_CONTENT);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        Response response = Response.builder()
+                .fecha(LocalDateTime.now().toString())
+                .codigoResultado("UNPROCESSABLE_ENTITY")
+                .descripcionRespuesta(errors.stream().reduce(" ", String::concat))
+                .result(Collections.emptyList())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
 
 
 
