60d54455336c96cf29681d24a12541dc9f7bfa74
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 11:50:42 -0500

    add: comments model

---
+++ domain/model/src/main/java/co/com/astropay/model/comments/Comments.java
@@ -0,0 +1,8 @@
+package co.com.astropay.model.comments;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class Comments {
+}
---
+++ domain/model/src/main/java/co/com/astropay/model/comments/gateways/CommentsRepository.java
@@ -0,0 +1,4 @@
+package co.com.astropay.model.comments.gateways;
+
+public interface CommentsRepository {
+}
