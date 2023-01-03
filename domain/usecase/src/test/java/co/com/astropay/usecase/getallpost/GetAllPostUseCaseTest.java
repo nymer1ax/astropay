a8ee7efcb88700360fc6ae1972ccfe54924cca02
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 15:57:37 -0500

    Create GetAllPostUseCaseTest.java

---
+++ domain/usecase/src/test/java/co/com/astropay/usecase/getallpost/GetAllPostUseCaseTest.java
@@ -0,0 +1,66 @@
+package co.com.astropay.usecase.getallpost;
+
+import co.com.astropay.model.post.Post;
+import co.com.astropay.model.post.gateways.PostRepository;
+import co.com.astropay.usecase.getallposttitle.GetAllPostTitleUseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class GetAllPostUseCaseTest {
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private GetAllPostTitleUseCase getAllPostTitleUseCase;
+
+    @InjectMocks
+    private GetAllPostUseCase getAllPostUseCase;
+
+    @Test
+    public void testValid() throws IOException {
+
+        Optional<String> title =Optional.empty();
+
+        Post p1 = Post.builder().title("klsdlsd").build();
+        Post p2 = Post.builder().title("aaaidsdsc").build();
+        List<Post> postList = List.of(p1, p2);
+
+        Mockito.when(postRepository.getAll()).thenReturn(postList);
+        List<Post> result = getAllPostUseCase.getAll(title);
+
+        Assertions.assertEquals(postList.size(), result.size());
+    }
+
+    @Test
+    public void testGetAllTitle() throws IOException {
+        Optional<String> title = Optional.of("sunt");
+
+        Post p1 = Post.builder().title("sunt 1").build();
+        Post p2 = Post.builder().title("sunt 2").build();
+        List<Post> postList = List.of(p1, p2);
+
+        Mockito.when(getAllPostTitleUseCase.getAllFilterByTitle(title.get())).thenReturn(postList);
+
+        List<Post> result = getAllPostUseCase.getAll(title);
+
+        Assertions.assertEquals(postList.size(), result.size());
+        Assertions.assertEquals(postList.get(0).getTitle(), result.get(0).getTitle());
+    }
+
+
+}
