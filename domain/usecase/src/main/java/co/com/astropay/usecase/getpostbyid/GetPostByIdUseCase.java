a07296b17f41bc2e519243436eecf336a44268de
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 13:31:38 -0500

    get post by id

--- domain/usecase/src/main/java/co/com/astropay/usecase/getpostbyid/GetPostByIdUseCase.java
+++ domain/usecase/src/main/java/co/com/astropay/usecase/getpostbyid/GetPostByIdUseCase.java
@@ -1,6 +1,21 @@
 package co.com.astropay.usecase.getpostbyid;
 
+import co.com.astropay.model.post.Post;
+import co.com.astropay.model.post.gateways.PostRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
 @RequiredArgsConstructor
 public class GetPostByIdUseCase {
+    private final PostRepository postRepository;
+
+    public Post getById(int id) throws IOException {
+        Post post = postRepository.getById(id);
+
+        if(post == null){
+            throw new RuntimeException("No hay contenido");
+        }
+        return post;
+    }
 }
--- infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/ApiRest.java
@@ -1,9 +1,11 @@
 package co.com.astropay.api;
 import co.com.astropay.model.post.Post;
 import co.com.astropay.usecase.getallpost.GetAllPostUseCase;
+import co.com.astropay.usecase.getpostbyid.GetPostByIdUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,17 @@ import java.util.List;
 public class ApiRest {
    private final GetAllPostUseCase getAllPostUseCase;
 
+   private final GetPostByIdUseCase getPostByIdUseCase;
 
     @GetMapping(path = "/posts")
-    public List<Post> commandName() throws IOException {
+    public List<Post> getAll() throws IOException {
         return getAllPostUseCase.getAll();
     }
+
+    @GetMapping(path = "/posts/{id}")
+    public Post getPost(@PathVariable(name = "id") int id) throws IOException {
+        return getPostByIdUseCase.getById(id);
+    }
+
+    
 }
