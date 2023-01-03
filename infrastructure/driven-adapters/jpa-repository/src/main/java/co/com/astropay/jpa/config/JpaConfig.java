1615e82685eefd4d08fdb00917bded4f3d9eae69
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 11:55:53 -0500

    configurar proyecto

--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/astropay/jpa/config/JpaConfig.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/astropay/jpa/config/JpaConfig.java
@@ -49,7 +49,7 @@ public class JpaConfig {
 
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", dialect);
-        properties.setProperty("hibernate.hbm2ddl.auto", "update"); // TODO: remove this for non auto create schema
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
         em.setJpaProperties(properties);
 
         return em;
