431eafae36a98c1e16de65fd63e4d05f3e4ea361
Ralph <ralph.sliger@imagineapps.co>
2023-01-03 13:23:24 -0500

    get all posts

--- domain/usecase/src/main/java/co/com/astropay/usecase/getallpost/GetAllPostUseCase.java
+++ domain/usecase/src/main/java/co/com/astropay/usecase/getallpost/GetAllPostUseCase.java
@@ -1,6 +1,23 @@
 package co.com.astropay.usecase.getallpost;
 
+import co.com.astropay.model.post.Post;
+import co.com.astropay.model.post.gateways.PostRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.util.List;
+
 @RequiredArgsConstructor
 public class GetAllPostUseCase {
+    private final PostRepository postRepository;
+
+    public List<Post> getAll() throws IOException {
+        List<Post> posts = postRepository.getAll();
+
+        if(posts.isEmpty()){
+            throw new RuntimeException("No hay contenido");
+        }
+
+        return posts;
+    }
 }
--- infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/astropay/api/ApiRest.java
@@ -1,20 +1,24 @@
 package co.com.astropay.api;
+import co.com.astropay.model.post.Post;
+import co.com.astropay.usecase.getallpost.GetAllPostUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ApiRest {
-//    private final MyUseCase useCase;
+   private final GetAllPostUseCase getAllPostUseCase;
 
 
-    @GetMapping(path = "/path")
-    public String commandName() {
-//      return useCase.doAction();
-        return "Hello World";
+    @GetMapping(path = "/posts")
+    public List<Post> commandName() throws IOException {
+        return getAllPostUseCase.getAll();
     }
 }
